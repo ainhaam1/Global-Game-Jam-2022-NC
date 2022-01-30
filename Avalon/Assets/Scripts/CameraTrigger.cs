c3ddbedfc5f49acab463a0c2a49f2f0a89fcb330
ZeinAlbahrani <57455654+ZeinAlbahrani@users.noreply.github.com>
2022-01-30 14:56:16 -0500

    Added cinemachine cuts

---
+++ Avalon/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public CinemachineVirtualCamera oldCamera;
+    public CinemachineVirtualCamera cameraToSwitch;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CameraSwitcher.Register(oldCamera);
+            CameraSwitcher.Register(cameraToSwitch);
+
+            if (CameraSwitcher.ActiveCamera != cameraToSwitch)
+            {
+                Debug.Log("camera switch here");
+                CameraSwitcher.SwitchCamera(cameraToSwitch);
+                CameraSwitcher.Unregister(oldCamera);
+            }
+        }
+    }
+}
